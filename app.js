aa4daabfad2448ea16c54dcc0f01386bf673b2e8
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2023-02-03 16:18:47 +0200

    added noticesSchema and notices routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
+const noticesRouter = require("./routes/api/notices");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -14,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use("/api/notices", noticesRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
