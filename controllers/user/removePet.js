39da4ea0227b09e9bf54a8f8b37ea5889b8b5436
Krystyna Sharhorodska <rinjara@gmail.com>
2023-02-05 00:23:39 +0200

    Added removePet

--- controllers/user/removePet.js
+++ controllers/user/removePet.js
@@ -1,3 +1,19 @@
-const removePet = async (req, res) => {};
+const { Pet } = require('../../models/petSchema');
+const createError = require('http-errors');
+
+const removePet = async (req, res) => {
+  const { id } = req.params;
+
+  const pet = await Pet.findByIdAndRemove(id);
+
+  if (!pet) {
+    throw new createError.NotFound(`Failure! pet with id: ${id} is not found`);
+  }
+
+  res.json({
+    message: `Pet with name ${pet.name} successfully deleted`,
+    id,
+  });
+};
 
 module.exports = removePet;
