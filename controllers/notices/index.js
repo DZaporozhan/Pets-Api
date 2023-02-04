dceeabe3d5be4f406408615bbe34fc603bdeffac
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-02-04 13:09:58 +0100

    adds adding favorite posts

--- controllers/notices/index.js
+++ controllers/notices/index.js
@@ -1,5 +1,6 @@
-const getOneNotice = require("./getOneNotice")
-
+const getOneNotice = require("./getOneNotice");
+const addFavoriteNotices = require("./addFavoriteNotices");
 module.exports = {
-    getOneNotice,
+  getOneNotice,
+  addFavoriteNotices,
 };
