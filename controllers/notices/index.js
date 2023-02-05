50bea883f83f1c90cc7759e8abad6841c5f7f569
aleksandr105 <aleksandrsherbina105@gmail.com>
2023-02-05 16:47:51 +0100

    adds controller and rout getAllUserNoties

--- controllers/notices/index.js
+++ controllers/notices/index.js
@@ -2,10 +2,12 @@ const add = require("./add");
 const addFavoriteNotices = require("./addFavoriteNotices");
 const getOneNotice = require("./getOneNotice");
 const getNoticeByCategory = require("./getNoticeByCategory");
+const getAllUserNotices = require("./getAllUserNotices");
 
 module.exports = {
   add,
   addFavoriteNotices,
   getOneNotice,
   getNoticeByCategory,
+  getAllUserNotices,
 };
