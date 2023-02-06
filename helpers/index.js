5a46feef5a1e8d55cb12ecc05d8e2ec6339ba14a
Dmytro <DSerSmart@gmail.com>
2023-02-06 13:59:18 +0200

    fix 409  removeNoticeFromFavorite

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const ctrlWrapper = require("./ctrlWrapper");
 const handleValidationError = require("./handleValidationError");
+const HttpError = require("./HttpError");
 
 module.exports = {
   ctrlWrapper,
   handleValidationError,
+  HttpError,
 };
