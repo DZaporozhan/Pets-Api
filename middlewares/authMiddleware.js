2300891cd47534be58d2c0791df229fb4610608d
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2023-02-07 21:12:40 +0200

    update

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 const { User } = require("../models/user");
 const createError = require("http-errors");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const { SECRET_KEY } = process.env;
 
