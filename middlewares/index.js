a88ae8c3a7dde4b182d73c632ca5c6bd15c59625
Krystyna Sharhorodska <rinjara@gmail.com>
2023-02-08 01:19:06 +0200

    pet-part swagger fix

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,10 @@
-const authMiddleware = require("./authMiddleware");
-const isValidId = require("./isValidId");
-const validation = require("./validation");
-const upload = require("./upload");
-const cloudinaryAddImage = require("./cloudinaryAddImage");
-const cloudinaryDeleteImage = require("./cloudinaryDeleteImage");
+// const validation = require("./validation");
+const authMiddleware = require('./authMiddleware');
+const isValidId = require('./isValidId');
+const validation = require('./validation');
+const upload = require('./upload');
+const cloudinaryAddImage = require('./cloudinaryAddImage');
+const cloudinaryDeleteImage = require('./cloudinaryDeleteImage');
 
 module.exports = {
   authMiddleware,
