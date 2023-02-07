da4cb56d55af45a6e75d0b3da274fae4600ea74d
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2023-02-07 21:23:39 +0200

    update

--- controllers/notices/getFavoriteNotices.js
+++ controllers/notices/getFavoriteNotices.js
@@ -1,25 +1,20 @@
 const { User } = require("../../models/user");
-const {Notices} = require("../../models/noticesSchema");
+const { Notices } = require("../../models/noticesSchema");
 const createError = require("http-errors");
 
+const getFavoriteNotices = async (req, res) => {
+  const { _id } = req.user;
+  const user = await User.findOne({ _id });
+  const idArray = user.favorite.map((favorite) => {
+    return favorite.toString();
+  });
+  const favorites = await Notices.find({ _id: idArray });
 
+  if (!favorites) {
+    throw new createError.NotFound(404, "You have no any favorite notices");
+  }
 
-
-const getFavoriteNotices = async (req, res) => { 
-    const { _id} = req.user;
-    const user = await User.findOne({ _id });
-    const idArray = user.favorite.map(favorite => { return favorite.toString() });
-    const favorites = await Notices.find({ _id: idArray });  
-
-   if (!favorites) {       
-        const error = new Error();
-         throw new createError.NotFound(404, 'You have no any favorite notices'); 
-    }
-   
-       res.status(200).json(favorites);
-    
-    
-   
-}
+  res.status(200).json(favorites);
+};
 
 module.exports = getFavoriteNotices;
