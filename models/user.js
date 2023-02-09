92d540d2267073b8f73da9d299975de24f22233c
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2023-02-09 11:48:37 +0200

    update regEx email

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const cityRegEx = /^([A-Za-z]+)([,][ ][A-Za-z]+)*$/;
 
 const emailRegexp =
   // eslint-disable-next-line no-useless-escape
-  /^(?=.{10,63}$)(([0-9A-Za-z][-0-9A-z\.]{1})@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/;
+  /^(?=.{10,63}$)(([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/;
 
 const userSchema = Schema(
   {
