3f300d748ab55d4b503cbda42c8856ae63b4a869
Illia-illia <takoexerrorx@gmail.com>
2023-02-09 14:14:37 +0200

    fix trycatch

--- middlewares/cloudinaryAddImage.js
+++ middlewares/cloudinaryAddImage.js
@@ -2,35 +2,39 @@ const { cloudinary } = require('../utils');
 const fs = require('fs/promises');
 
 const cloudinaryAddImage = async (req, res, next) => {
-  if (!req.file) {
+  try {
+    if (!req.file) {
+      next();
+      return;
+    }
+
+    const { category, birthdate } = req.body;
+
+    const { path: imagePath } = req.file;
+
+    const bitmap = await fs.readFile(imagePath, 'base64');
+    const imageURL = `data:image/jpeg;base64,${bitmap}`;
+    fs.unlink(imagePath);
+
+    let folder = '';
+
+    if (category) {
+      folder = 'Pets';
+    } else if (birthdate) {
+      folder = 'Avatars';
+    } else {
+      folder = 'Users';
+    }
+
+    const uploadedResponse = await cloudinary.uploader.upload(imageURL, {
+      folder,
+      transformation: { width: 250, crop: 'scale' },
+    });
+    req.body.imageURL = uploadedResponse.public_id;
     next();
-    return;
+  } catch (error) {
+    next();
   }
-
-  const { category, birthdate } = req.body;
-
-  const { path: imagePath } = req.file;
-
-  const bitmap = await fs.readFile(imagePath, 'base64');
-  const imageURL = `data:image/jpeg;base64,${bitmap}`;
-  fs.unlink(imagePath);
-
-  let folder = '';
-
-  if (category) {
-    folder = 'Pets';
-  } else if (birthdate) {
-    folder = 'Avatars';
-  } else {
-    folder = 'Users';
-  }
-
-  const uploadedResponse = await cloudinary.uploader.upload(imageURL, {
-    folder,
-    transformation: { width: 250, crop: 'scale' },
-  });
-  req.body.imageURL = uploadedResponse.public_id;
-  next();
 };
 
 module.exports = cloudinaryAddImage;
