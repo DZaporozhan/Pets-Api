3c4e15863d643d2b6248b0fd8e79b939db2abc3b
Illia-illia <takoexerrorx@gmail.com>
2023-02-13 10:46:16 +0200

    title-search finish

--- controllers/notices/getNoticeByCategory.js
+++ controllers/notices/getNoticeByCategory.js
@@ -2,12 +2,21 @@ const { Notices } = require('../../models/noticesSchema');
 const createError = require('http-errors');
 
 const getNoticeByCategory = async (req, res) => {
-  const { category, page = 1, limit = 10 } = req.query;
+  const { category, page = 1, limit = 10, title } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Notices.find({ category }, '-createdAt -updatedAt', {
+
+  let result = await Notices.find({ category }, '-createdAt -updatedAt', {
     skip,
     limit,
   });
+
+  if (title) {
+    const facts = await Notices.find({ category }, '-createdAt -updatedAt');
+    const factsFilter = facts.filter((fact) =>
+      fact.title.toLowerCase().includes(title.toLowerCase())
+    );
+    result = factsFilter;
+  }
 
   const categoryList = ['sell', 'lost found', 'in good hands'];
   const key = categoryList.includes(category);
@@ -18,6 +27,13 @@ const getNoticeByCategory = async (req, res) => {
   if (!result) {
     throw new createError.NotFound('not found');
   }
+
+  if (!result.length) {
+    throw new createError.NotFound(
+      `Bad Request: Notices with title = ${title} not found!`
+    );
+  }
+
   res.json({
     status: 'Success',
     code: 200,
